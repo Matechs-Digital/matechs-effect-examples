a904b16ca95898e6374eff366c25ac35c90f1bb6
Michael Arnaldi <arnaldimichael@gmail.com>
2020-03-09 23:44:29 +0000

    bootstrap

---
+++ api-client/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  rootDir: "./",
+  transform: {
+    "^.+\\.ts?$": "ts-jest"
+  }
+};
---
+++ api-client/tests/api.test.ts
@@ -0,0 +1,8 @@
+import * as assert from "assert";
+import { a } from "../src";
+
+describe("ApiClient", () => {
+  it("should", () => {
+    assert.deepEqual(a, 1);
+  });
+});
